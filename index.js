bd7f2a05ea7470064f9133ecc405b98fc77964ad
Alyona Ivanushkina <alyona.ivanushkina@gmail.com>
2021-01-02 01:24:16 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,13 +1,19 @@
 import express from 'express';
 import bodyParser from 'body-parser';
-import {createReadStream} from 'fs';
-import crypto from 'crypto';
-import http from 'http';
-import Zombie from 'zombie';
 import mongodb from 'mongodb';
 import cors from 'cors';
+import puppeteer from 'puppeteer';
+
 import path from 'path';
+import fs from 'fs';
+import crypto from 'crypto';
+import http from 'http';
 
 import appSrc from './app.js';
-const app = appSrc(express, bodyParser, createReadStream, crypto, http, mongodb, Zombie, cors, path);
-app.listen(process.env.PORT || 3400);
+
+const app = appSrc(express, bodyParser, fs, crypto, http, mongodb, path, cors, puppeteer);
+
+app.listen(process.env.PORT ?? 4321);
+app.listen(port, () => {
+  console.log('Server is up!');
+});
