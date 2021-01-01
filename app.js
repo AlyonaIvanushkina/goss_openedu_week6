f12cc205ccfa39d1905385cc72f6e26e3c5925a2
Alyona Ivanushkina <alyona.ivanushkina@gmail.com>
2021-01-01 19:04:27 +0700

    Update app.js

--- app.js
+++ app.js
@@ -67,39 +67,42 @@
 //   return app;
 // }
 export default function appSrc(express, bodyParser, createReadStream, crypto, http, mongo) {
-const app = express();
-const CORS = {
-'Access-Control-Allow-Origin': '*',
-'Access-Control-Allow-Methods': 'GET,POST,PUT,PATCH,OPTIONS,DELETE»',
-'Content-Type': 'text/plain; charset=utf-8'
-};
-const login = "itmo182954";
-app
-.use(bodyParser.urlencoded({ extended: true }))
-.all('/insert/', (req, res) => {
-res.set(CORS);
-if (!!req.body.URL && !!req.body.login && !!req.body.password) {
-const {MongoClient} = mongo;
-const client = new MongoClient(req.body.URL, { useNewUrlParser: true, useUnifiedTopology: true });
+  const app = express();
+  
+  const CORS = {
+    'Access-Control-Allow-Origin': '*',
+    'Access-Control-Allow-Methods': 'GET,POST,PUT,PATCH,OPTIONS,DELETE»',
+    'Content-Type': 'text/plain; charset=utf-8'
+  };
+  
+  const login = "itmo182954";
+  
+  app.use(bodyParser.urlencoded({ extended: true }))
+  .all('/insert/', (req, res) => {
+    res.set(CORS);
+    if (!!req.body.URL && !!req.body.login && !!req.body.password) {
+      const {MongoClient} = mongo;
+      const client = new MongoClient(req.body.URL, { useNewUrlParser: true, useUnifiedTopology: true });
 
-async function run() {
-try {
-await client.connect();
-const result = await client.db().collection('users').insertOne({
-login: req.body.login.toString(),
-password: req.body.password.toString()
-})
-await client.close();
-res.send(result);
-} catch (err) {
-res.send(`Something went wrong: ${err}`);
-} finally {
-await client.close();
-res.send();
+      async function run() {
+        try {
+           await client.connect();
+           const result = await client.db().collection('users').insertOne({
+           login: req.body.login.toString(),
+           password: req.body.password.toString()
+           })
+           await client.close();
+           res.send(result);
+         } catch (err) {
+           res.send(`Something went wrong: ${err}`);
+         } finally {
+           await client.close();
+           res.send();
+         }
+      }
+       run().catch();
+    } else {
+      res.send(login);
+    }
+  })
 }
-}
-run().catch();
-} else {
-res.send(login);
-}
-})
