8c3463f134bfaee745427389ed3aee9269864aa3
Alyona Ivanushkina <alyona.ivanushkina@gmail.com>
2021-01-01 21:33:17 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,19 @@
+ 
 import express from 'express';
 import bodyParser from 'body-parser';
-import {createReadStream} from 'fs';
+import mongodb from 'mongodb';
+import cors from 'cors';
+
+import path from 'path';
+import fs from 'fs';
 import crypto from 'crypto';
 import http from 'http';
-import mongodb from 'mongodb';
 
 import appSrc from './app.js';
-const app = appSrc(express, bodyParser, createReadStream, crypto, http, mongodb);
-app.listen(process.env.PORT || 3400);
+
+const app = appSrc(express, bodyParser, fs, crypto, http, mongodb, path, cors);
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+  console.log('Server is up!');
+});
